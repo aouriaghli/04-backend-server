075c686bbbd7007ed47acb69b3012fc14a3f6dac
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-08-16 13:40:37 +0200

    Sec10.  Encriptar contraseña (bcryptjs)

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,6 @@
 const Usuario = require('../models/usuario');
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 
 const getUsuarios = async(req, res) => {
    
@@ -14,7 +15,7 @@ const getUsuarios = async(req, res) => {
 
 const crearUsuarios = async(req, res = response) => {
    
-    const {nombre, password, email } = req.body;
+    const { password, email } = req.body;
 
     try {
 
@@ -28,6 +29,10 @@ const crearUsuarios = async(req, res = response) => {
         }
 
         const usuario = new Usuario( req.body);
+
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
 
         await usuario.save();  
         
